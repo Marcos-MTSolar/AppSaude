34dbcfefe345741f4bb270a56cd64b133e8158f4
Marcos Nascimento <aurelio.marcos21@gmail.com>
2026-06-21 14:49:11 -0300

    fix(android): force clear WebView cache on app startup

--- android/app/src/main/java/com/marcosesandra/vidasaudavel/MainActivity.java
+++ android/app/src/main/java/com/marcosesandra/vidasaudavel/MainActivity.java
@@ -1,5 +1,23 @@
 package com.marcosesandra.vidasaudavel;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = this.bridge.getWebView();
+        if (webView != null) {
+            // Limpa o cache atual (disco e memória)
+            webView.clearCache(true);
+            
+            // Força o WebView a não usar cache para requisições futuras
+            WebSettings settings = webView.getSettings();
+            settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
+    }
+}
